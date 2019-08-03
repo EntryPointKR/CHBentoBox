4b88bd61e7e7f7273e8d42c57acbd92f4f323b88
EntryPointKR <entrypoint_@naver.com>
2019-08-03 22:01:19 +0900

    Set priority to LOWEST

--- src/main/java/kr/entree/chbentobox/processor/BentoBoxEventNotifier.java
+++ src/main/java/kr/entree/chbentobox/processor/BentoBoxEventNotifier.java
@@ -5,6 +5,7 @@ import com.laytonsmith.core.events.EventUtils;
 import kr.entree.chbentobox.event.CHBentoBoxIslandEvent;
 import kr.entree.chbentobox.event.adapter.IslandEventAdapter;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import world.bentobox.bentobox.api.events.island.IslandEvent;
 
@@ -12,7 +13,7 @@ import world.bentobox.bentobox.api.events.island.IslandEvent;
  * Created by JunHyeong Lim on 7/30/2019
  */
 public class BentoBoxEventNotifier implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onIslandEvent(IslandEvent e) {
         IslandEventAdapter event = new IslandEventAdapter(e);
         EventUtils.TriggerListener(Driver.EXTENSION, CHBentoBoxIslandEvent.NAME, event);
